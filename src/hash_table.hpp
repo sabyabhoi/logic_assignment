8f603dbe37d5d65d0f7ef46cda6755f7abbe6b3e
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2022-10-17 11:51:41 +0530

    Add documentation for Hash Table

--- src/hash_table.hpp
+++ src/hash_table.hpp
@@ -3,9 +3,20 @@
 
 #define NULL_INT -1
 
+namespace hash_table {
+
+/*!
+ * @brief Hash table data structure implementation
+ *
+ * This is a simple implementation of a hash table, which will
+ * take a character as a key, and an integer as a value, and map the
+ * character to the integer using a hash function.
+ */
 struct HashTable {
 private:
+  /// @brief size of the hash table
   unsigned int size;
+  /// @brief the actual table where the data will be stored
   int* table;
 
   unsigned int hash(char);
@@ -14,8 +25,9 @@ public:
   HashTable(unsigned int s);
   ~HashTable();
   
-  bool insert(char);
+  bool insert(char, int);
   int at(char);
 
   void print_table();
 };
+}
