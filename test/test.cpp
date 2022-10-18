9b3d734cfcdfc5b1d06ca4ded97d8de263b5a5f2
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2022-10-18 13:50:42 +0530

    Add better styles for Doxygen

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,22 @@
+#include "stack.hpp"
 #include "gtest/gtest.h"
+
+TEST(StackTest, PushPopValues) {
+  stack::Stack st;
+
+  st.push('A');
+  EXPECT_EQ(st.top(), 'A');
+  st.push('B');
+  EXPECT_EQ(st.top(), 'B');
+  st.push('C');
+  EXPECT_EQ(st.top(), 'C');
+  EXPECT_EQ(st.size, 3);
+
+  st.pop();
+  st.pop();
+  st.pop();
+  EXPECT_EQ(st.empty(), true);
+}
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
