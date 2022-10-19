49388aabf69085dd63e279b49bdb38f0a50d8169
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2022-10-19 12:55:18 +0530

    Move parse tree tests into separate file

--- test/parse_tree_test.cpp
+++ test/parse_tree_test.cpp
@@ -1,26 +1,8 @@
 #include "parse_tree.hpp"
-#include "stack.hpp"
-#include "utils.hpp"
 #include "gtest/gtest.h"
 
-TEST(StackTest, PushPopValues) {
-  stack::Stack st;
 
-  st.push('A');
-  EXPECT_EQ(st.top(), 'A');
-  st.push('B');
-  EXPECT_EQ(st.top(), 'B');
-  st.push('C');
-  EXPECT_EQ(st.top(), 'C');
-  EXPECT_EQ(st.size, 3);
-
-  st.pop();
-  st.pop();
-  st.pop();
-  EXPECT_EQ(st.empty(), true);
-}
-
-/*TEST(ParseTreeTest, InfixToPrefix) {
+TEST(ParseTreeTest, InfixToPrefix) {
   using namespace parse_tree;
   EXPECT_EQ(infixToPrefix("A+B"), "+AB");
   EXPECT_EQ(infixToPrefix("(A+B)"), "+AB");
@@ -82,18 +64,5 @@ TEST(ParseTreeTest, ImplicationEval) {
   vals[char_index('B')] = 1;
   EXPECT_EQ(evaluate(head, vals), 1);
   deleteTree(head);
-  } */
-
-TEST(UtilsTest, CharIndex) {
-  EXPECT_EQ(char_index('A'), 0);
-  EXPECT_EQ(char_index('B'), 1);
-  EXPECT_EQ(char_index('Z'), 25);
-  EXPECT_EQ(char_index('a'), 0);
-  EXPECT_EQ(char_index('b'), 1);
-  EXPECT_EQ(char_index('z'), 25);
 }
 
-int main(int argc, char *argv[]) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
